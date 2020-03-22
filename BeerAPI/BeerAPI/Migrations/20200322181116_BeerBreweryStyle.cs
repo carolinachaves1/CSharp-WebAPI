0404e317e10c4c465f52774dea2ffd36a17d50b7
Carolina Amaral Chaves <cchaves@ciandt.com>
2020-03-22 15:20:15 -0300

    First Migrations e Update-Database

---
+++ BeerAPI/BeerAPI/Migrations/20200322181116_BeerBreweryStyle.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BeerAPI.Migrations
+{
+    public partial class BeerBreweryStyle : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Breweries",
+                columns: table => new
+                {
+                    BreweryId = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    Country = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Breweries", x => x.BreweryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Styles",
+                columns: table => new
+                {
+                    StyleId = table.Column<Guid>(nullable: false),
+                    StyleName = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Styles", x => x.StyleId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Beers",
+                columns: table => new
+                {
+                    BeerId = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    BitternessUnits = table.Column<float>(nullable: false),
+                    AlcoholVolume = table.Column<float>(nullable: false),
+                    created_at = table.Column<DateTime>(nullable: false),
+                    updated_at = table.Column<DateTime>(nullable: false),
+                    BreweryId = table.Column<Guid>(nullable: false),
+                    StyleId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Beers", x => x.BeerId);
+                    table.ForeignKey(
+                        name: "FK_Beers_Breweries_BreweryId",
+                        column: x => x.BreweryId,
+                        principalTable: "Breweries",
+                        principalColumn: "BreweryId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Beers_Styles_StyleId",
+                        column: x => x.StyleId,
+                        principalTable: "Styles",
+                        principalColumn: "StyleId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Beers_BreweryId",
+                table: "Beers",
+                column: "BreweryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Beers_StyleId",
+                table: "Beers",
+                column: "StyleId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Beers");
+
+            migrationBuilder.DropTable(
+                name: "Breweries");
+
+            migrationBuilder.DropTable(
+                name: "Styles");
+        }
+    }
+}
